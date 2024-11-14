5690b3270f64ede25eb13261210a39ffd0351f63
keithwalsh <keithwalsh@gmail.com>
2024-11-14 13:16:45 +0000

    chore: configure vite for library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "ReactSpreadsheetTs",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: [
+        "react", 
+        "react-dom", 
+        "@emotion/react", 
+        "@emotion/styled",
+        "@mui/material",
+        "react-icons",
+        "@mui/icons-material"
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@emotion/react": "emotionReact",
+          "@emotion/styled": "emotionStyled",
+          "@mui/material": "MuiMaterial",
+          "react-icons": "ReactIcons",
+          "@mui/icons-material": "MuiIcons"
+        }
+      }
+    }
+  }
 });
