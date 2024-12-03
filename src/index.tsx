98377aa408057ac1ac127984278ba7f9c2fad441
keithwalsh <keithwalsh@gmail.com>
2024-12-03 21:09:33 +0000

    feat: add CellFormat type to exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-import Spreadsheet from './components/Spreadsheet';
-export type { SpreadsheetProps } from './types';
-export default Spreadsheet;
+import { Spreadsheet } from './components/Spreadsheet'
+export type { SpreadsheetProps, CellFormat } from './types'
+export { Spreadsheet }
